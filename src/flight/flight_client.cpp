93b589312fe6d80f983633f6ebdc48583689096b
Bill Guowei Yang <bill@posthog.com>
2026-02-11 16:19:36 -0500

    add back accidentally removed ensure_alignment

--- src/flight/flight_client.cpp
+++ src/flight/flight_client.cpp
@@ -154,6 +154,9 @@ arrow::flight::FlightCallOptions PostHogFlightClient::GetCallOptions() const {
 	// Control new allocations Arrow performs while decoding.
 	options.memory_manager = arrow::default_cpu_memory_manager();
 	options.read_options = arrow::ipc::IpcReadOptions::Defaults();
+	// Ensure Flight buffers are aligned even when gRPC buffers are misaligned.
+	options.read_options.ensure_alignment = arrow::ipc::Alignment::kDataTypeSpecificAlignment;
+
 
 	return options;
 }
